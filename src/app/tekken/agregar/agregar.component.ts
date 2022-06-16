fdb55dba2578ee66f028cf531d998b7258350f89
eduardojrl927@gmail.com <eduardojrl927@gmail.com>
2022-06-16 13:18:26 +0200

    agregar.component added

---
+++ src/app/tekken/agregar/agregar.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styleUrls: ['./agregar.component.css']
+})
+export class AgregarComponent{
+
+  @Input('agregar') agregar: void | undefined;
+
+}
